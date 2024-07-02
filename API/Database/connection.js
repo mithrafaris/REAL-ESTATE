56b6e845d431daca82c1f4b4d265f788aab5059a
mithra faris <mithrafaris31@gmail.com>
2024-07-03 01:35:33 +0530

    created model

--- API/Database/connection.js
+++ API/Database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const con = await mongoose.connect(process.env.MONGO_URL); 
-    console.log(`MongoDB connection: ${con.connection.host}`);
+    console.log(`MongoDB connection`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
