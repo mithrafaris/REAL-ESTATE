d5857842dac3b9cceba3794b68c86377ae045d0d
mithra faris <mithrafaris31@gmail.com>
2024-08-20 22:23:10 +0530

    update it

--- API/Route/userRoute/userRoutes.js
+++ API/Route/userRoute/userRoutes.js
@@ -16,7 +16,7 @@ router.get('/:id', verifyToken, getUser);
 
 // Listing Routes
 router.post('/create', verifyToken, createListing);
-router.get('/listing/:id', verifyToken, getUserListings);//full userss
+router.get('/listing/:id', verifyToken, getUserListings);//users list
 router.delete('/deletelisting/:id', verifyToken, deleteListing);
 router.post('/updated/:id', verifyToken, updateListing);
 router.get('/getlisting/:id', getListing);//single user
