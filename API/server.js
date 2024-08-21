6ebea9b932061b54d2e1a4a2ad2703248a030ea0
mithra faris <mithrafaris31@gmail.com>
2024-08-21 20:39:05 +0530

    server update

--- API/server.js
+++ API/server.js
@@ -16,9 +16,10 @@ connectDB();
 
 app.use('/user', userRoute);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// Corrected path to static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.use((err, req, res, next) => {
